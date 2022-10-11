a793745a7f82f3f73d6a01bea7614e3fe4ae0bc3
naumov_nikita <aeromusician@yandex.ru>
2022-10-11 11:34:26 +0300

    Initial commit

---
+++ app/src/main/java/it/mirea/armhrdep/MainActivity.java
@@ -0,0 +1,26 @@
+package it.mirea.armhrdep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+}
